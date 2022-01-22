d5ad30a1620a3f5849059742e8b46d9c5fd62903
Sihyun <tjtlgus4@naver.com>
2022-01-23 01:38:28 +0900

    clone-coding(chap03_4)
    basic flex, alignItem, justifyContent

---
+++ ch03_4/imagePath.js
@@ -0,0 +1,6 @@
+export const imagePath = {
+    first : require('./src/assets/images/image1.jpeg'),
+    second : require('./src/assets/images/image2.jpeg'),
+    third : require('./src/assets/images/image3.jpeg'),
+    forth : require('./src/assets/images/image4.jpeg')
+}
