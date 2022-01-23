067929512bc5b67ec5b6f3d59e5948fe0910bb21
Sihyun <tjtlgus4@naver.com>
2022-01-23 14:53:16 +0900

    delete chap03_3, 4
    delete two clone-coding project because of watchman issue

--- ch03_4/imagePath.js
+++
@@ -1,6 +0,0 @@
-export const imagePath = {
-    first : require('./src/assets/images/image1.jpeg'),
-    second : require('./src/assets/images/image2.jpeg'),
-    third : require('./src/assets/images/image3.jpeg'),
-    forth : require('./src/assets/images/image4.jpeg')
-}
